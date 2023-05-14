f499638db55f7f8a1761d76c8f80c7af68974984
S0haibZafar <sohaibzafar71@gmail.com>
2023-05-14 16:04:59 +0500

    Test app.js / path

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const app = express();
 
 const port = process.env.port || 8080
 
+app.get("/", (req, res)=>{
+    res.send("Wellcome To Server::::+😊");
+});
+
 app.listen(port, ()=>{
         console.log("Node Server is connected: ", port);
 });
