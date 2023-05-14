5d75423c3cd18ace4a8b3585353cb09f835d9198
S0haibZafar <sohaibzafar71@gmail.com>
2023-05-14 16:21:16 +0500

    Import register and login routes in app and bind with /auth

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ const express = require("express")
 
 const app = express();
 
-const port = process.env.port || 8080
+const port = process.env.port || 8080;
+
+const authRoute = require("./routes/auth-route");
+// var routes = require("./routes/index.js");
+
+app.use('/auth', authRoute);
 
 app.get("/", (req, res)=>{
     res.send("Wellcome To Server::::+😊");
