525703e72834defe7d7bfa056bfb7f0c0a454a08
S0haibZafar <sohaibzafar71@gmail.com>
2023-05-14 16:50:37 +0500

    Create user model with mongoose schema and import in /register route

--- routes/auth-route.js
+++ routes/auth-route.js
@@ -1,7 +1,13 @@
 const router = require("express").Router();
+const User = require("../models/user");
 
 router.post("/register", (req, res)=>{
-    res.json("Register works");
+    // res.json("Register works");
+    const user = new User({
+        name : req.body.name,
+        email : req.body.email,
+        password : req.body.password,
+    }); 
 });
 
 router.post("/login", (req, res)=>{
