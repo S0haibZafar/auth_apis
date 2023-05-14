8c009384710a9a8b1848d950795fc94fdf067996
S0haibZafar <sohaibzafar71@gmail.com>
2023-05-14 15:46:26 +0500

    Create/import express and make basic flow

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express")
+
+
+const app = express();
+
+const port = process.env.port || 8080
+
+app.listen(port, ()=>{
+        console.log("Node Server is connected: ", port);
+});
