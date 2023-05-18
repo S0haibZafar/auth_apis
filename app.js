e661d0415c1adcc724ccdae76f9cd41231de6fe4
S0haibZafar <sohaibzafar71@gmail.com>
2023-05-18 08:08:04 +0500

    Create connection with mongoDB

--- app.js
+++ app.js
@@ -9,14 +9,19 @@ const authRoute = require("./routes/auth-route");
 // var routes = require("./routes/index.js");
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/myapp');
+mongoose.connect('mongodb://127.0.0.1:27017/auth-app')
+    .then(() => {
+        console.log("DB is connected...");
+    }).catch((err) => {
+        console.log("DB is not connected!!", err);
+    });
 
 app.use('/auth', authRoute);
 
-app.get("/", (req, res)=>{
+app.get("/", (req, res) => {
     res.send("Wellcome To Server::::+😊");
 });
 
-app.listen(port, ()=>{
-        console.log("Node Server is connected: ", port);
+app.listen(port, () => {
+    console.log("Node Server is connected: ", port);
 });
