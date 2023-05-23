9fd565e338d362976caa15653871cb54a282cba5
S0haibZafar <sohaibzafar71@gmail.com>
2023-05-23 07:21:26 +0500

    Adjust checkAtuh middleware

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-    const token = req.headers.authorization.split('')[1];
+    const token = req.headers.authorization.split(' ')[1];
     return res.json(token);
 } 
