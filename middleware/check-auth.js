3e8d09593e959802ed3217a05b043cba14d0f95f
S0haibZafar <sohaibzafar71@gmail.com>
2023-05-23 07:28:13 +0500

    Adjust middle ware according to all apis

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -2,5 +2,8 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
     const token = req.headers.authorization.split(' ')[1];
-    return res.json(token);
+    // return res.json(token);
+    const decode = jwt.verify(token, 'webBatch');
+    req.userData = decode
+    return decode;
 } 
